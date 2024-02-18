f4805118fa64270f71e5a1a2afab90933941fe88
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-18 20:35:53 +0530

    lowePart of diamond is add

--- Diamond.java
+++ Diamond.java
@@ -12,5 +12,17 @@ public class Diamond {
             }
             System.out.println();
         }
+        // reverse the upperPart
+        for (int i = 6; i >= 1; i--) {
+            // Space print
+            for (int k = 7; k > i; k--) {
+                System.out.print(" ");
+            }
+            // star print
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
