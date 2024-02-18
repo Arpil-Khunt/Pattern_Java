36d58b777da07626b437bab41c5e53feb934ef8f
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-18 20:31:50 +0530

    UpperPart print

--- Diamond.java
+++ Diamond.java
@@ -1,5 +1,16 @@
 public class Diamond {
     public static void main(String args[]) {
-
+        // add new line
+        for (int i = 1; i <= 7; i++) {
+            // Space print
+            for (int k = 7; k > i; k--) {
+                System.out.print(" ");
+            }
+            // star print
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
