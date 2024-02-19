80e2cd114900a1335790de35c079a1579d8ad2f6
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-19 22:35:33 +0530

    Pattern add

---
+++ Pyramid_full.java
@@ -0,0 +1,19 @@
+public class Pyramid_full {
+
+    /*
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+     */
+
+    public static void main(String args[]) {
+        // print above pattern
+    }
+}
