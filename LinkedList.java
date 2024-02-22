4fd28733be892fa3c08a0ec6938bf12db5c5ed86
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-22 07:35:48 +0530

    LinkedList file added

---
+++ LinkedList.java
@@ -0,0 +1,13 @@
+public class LinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static Node head;
+    static Node tail;
+}
