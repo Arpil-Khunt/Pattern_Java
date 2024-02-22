7fe817fb9d3370c9f9d707034e26d76e9cc81171
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-22 21:30:30 +0530

    Inverted Hollow Triangle added

--- Hollow_Triangle.java
+++ Hollow_Triangle.java
@@ -1,7 +1,7 @@
 public class Hollow_Triangle {
 
     public static void main(String args[]) {
-        int x = 0;
+
         for (int i = 0; i < 5; i++) {
             for (int k = 4; k > i; k--) {
                 System.out.print(" ");
@@ -15,6 +15,28 @@ public class Hollow_Triangle {
                 }
             }
             if (i == 0) {
+                System.out.println();
+            } else {
+                System.out.println("*");
+            }
+
+        }
+        // inverted hollow triangle
+        for (int i = 5; i > 0; i--) {
+            // space add begin
+            for (int k = 4; k >= i; k--) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            // space add in between star
+            for (int j = 0; j < (2 * i) - 3; j++) {
+                if (i != 5) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            if (i == 1) {
                 System.out.println();
             } else {
                 System.out.println("*");
