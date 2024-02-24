b7a9a215f7f3080250a3e4bf302575f0905d0a24
arpil_khunt <arpilkhunt1203@gmail.com>
2024-02-24 16:30:15 +0530

    In DoublyLinkedList Insert at any position, method added

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,4 +1,6 @@
 public class DoublyLinkedList {
+    int size;
+
     class Node {
         int data;
         Node next;
@@ -8,12 +10,30 @@ public class DoublyLinkedList {
             this.data = data;
             this.next = null;
             this.prev = null;
+            size++;
+        }
+
+        Node(int data, Node prev) {
+            this.data = data;
+            this.prev = prev;
+        }
+
+        Node(Node next, int data) {
+            this.data = data;
+            this.next = next;
+        }
+
+        Node(int data, Node prev, Node next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
         }
     }
 
     Node head;
     Node tail;
 
+    // Insert at First
     void addFirst(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -26,6 +46,7 @@ public class DoublyLinkedList {
         head = newNode;
     }
 
+    // Insert at Last
     void addLast(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -37,6 +58,25 @@ public class DoublyLinkedList {
         tail = newNode;
     }
 
+    // Insert at any Index
+    public void addAtIndex(int data, int Index) {
+
+        if (head.next == null || Index == 0) {
+            addFirst(data);
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < Index; i++) {
+            temp = temp.next;
+        }
+        Node temp1 = temp.next;
+        Node newNode = new Node(data, temp, temp1);
+        temp1.prev = newNode;
+        temp.next = newNode;
+
+    }
+
+    // Print Forward LinkedList
     void ForwardPrintLL() {
         if (head == null) {
             System.out.println("LL is empty");
@@ -51,6 +91,7 @@ public class DoublyLinkedList {
 
     }
 
+    // Print Backward LinkedList
     void ReversePrintLL() {
         if (head == null) {
             System.out.println("LL is empty");
@@ -68,11 +109,11 @@ public class DoublyLinkedList {
         DoublyLinkedList ll = new DoublyLinkedList();
         ll.addFirst(56);
         ll.addFirst(55);
-        ll.addFirst(53);
-        ll.addFirst(52);
-        ll.addLast(57);
         ll.addLast(58);
+        ll.addAtIndex(54, 0);
+        ll.addAtIndex(57, 3);
         ll.ForwardPrintLL();
         ll.ReversePrintLL();
+
     }
 }
